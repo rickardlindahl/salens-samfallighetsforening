76bdd10a655fd1ee4eae55cef60541f32cf105cd
Rickard Lindahl <rickard@lindahl.app>
2023-09-05 21:01:05 +0200

    Add types to supabase in app.d.ts

--- src/app.d.ts
+++ src/app.d.ts
@@ -1,9 +1,10 @@
 import { SupabaseClient, Session } from "@supabase/supabase-js";
+import type { Database } from "./types/supabase";
 
 declare global {
   namespace App {
     interface Locals {
-      supabase: SupabaseClient;
+      supabase: SupabaseClient<Database>;
       getSession(): Promise<Session | null>;
     }
     interface PageData {
