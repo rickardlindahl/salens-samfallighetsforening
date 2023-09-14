4c94e8673a62266f92f85f40f60fc9f5e75833ef
Rickard Lindahl <rickard@lindahl.app>
2023-09-14 14:02:42 +0200

    Move password and loginForm zod schema to lib/schema

--- src/lib/schema.ts
+++ src/lib/schema.ts
@@ -20,3 +20,12 @@ export const deleteDocumentSchema = z.object({
 });
 
 export type DeleteDocumentSchema = typeof deleteDocumentSchema;
+
+export const passwordSchema = z.string().min(8, "Lösenordet måste vara minst 8 tecken långt");
+
+export const loginFormSchema = z.object({
+  email: z.string().email("Var god fyll i en giltig epost-adress"),
+  password: passwordSchema,
+});
+
+export type LoginFormSchema = typeof loginFormSchema;
