9e7ead289a68b90099b596c31e751648b308e9b5
Rickard Lindahl <rickard@lindahl.app>
2023-09-20 06:50:39 +0200

    Update login schema to avoid password errors
    It feels weird to get an error when typing in the password when logging in.

--- src/lib/schema.ts
+++ src/lib/schema.ts
@@ -25,7 +25,7 @@ export const passwordSchema = z.string().min(8, "Lösenordet måste vara minst 8
 
 export const loginFormSchema = z.object({
   email: z.string().email("Var god fyll i en giltig epost-adress"),
-  password: passwordSchema,
+  password: z.string(),
 });
 
 export type LoginFormSchema = typeof loginFormSchema;
