4b73b92b18e52750d78cf1ba8cf66b5ad8a168f1
Rickard Lindahl <rickard@lindahl.app>
2023-09-27 11:30:22 +0200

    Add distinct webhook payload schemas (#36)
    insert payloads does not contain an old record.

--- src/lib/webhook.ts
+++ src/lib/webhook.ts
@@ -1,17 +1,30 @@
 import { z } from "zod";
 import type { PostSchema, DocumentSchema } from "./zod-schema";
 
-export const webhookPayloadSchema = <T extends PostSchema | DocumentSchema>({
+export const webhookInsertPayloadSchema = <T extends PostSchema | DocumentSchema>({
   record,
   table,
-  type,
 }: {
   record: T;
   table: "posts" | "documents";
-  type: "INSERT" | "UPDATE" | "DELETE";
 }) =>
   z.object({
-    type: z.literal(type),
+    type: z.literal("INSERT"),
+    table: z.literal(table),
+    schema: z.string(),
+    record,
+    old_record: z.null(),
+  });
+
+export const webhookUpdatePayloadSchema = <T extends PostSchema | DocumentSchema>({
+  record,
+  table,
+}: {
+  record: T;
+  table: "posts" | "documents";
+}) =>
+  z.object({
+    type: z.literal("UPDATE"),
     table: z.literal(table),
     schema: z.string(),
     record,
--- src/routes/api/webhooks/documents-insert/+server.ts
+++ src/routes/api/webhooks/documents-insert/+server.ts
@@ -3,7 +3,7 @@ import { documentSchema, type Document } from "$lib/zod-schema";
 import type { SupabaseClient } from "@supabase/supabase-js";
 import { error, json, type RequestHandler } from "@sveltejs/kit";
 import type { Database } from "../../../../types/supabase";
-import { webhookPayloadSchema } from "$lib/webhook";
+import { webhookInsertPayloadSchema } from "$lib/webhook";
 
 type SendDocumentUploadedEmail = {
   fetch(input: RequestInfo | URL, init?: RequestInit | undefined): Promise<Response>;
@@ -75,9 +75,8 @@ export const POST: RequestHandler = async ({ fetch, request, locals: { supabase 
   const body = await request.json();
 
   try {
-    const data = webhookPayloadSchema({
+    const data = webhookInsertPayloadSchema({
       table: "documents",
-      type: "INSERT",
       record: documentSchema,
     }).parse(body);
 
