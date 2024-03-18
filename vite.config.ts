62834331c06d8e173fb8e47c6dae8b2037a71d18
Rickard Lindahl <rickard@lindahl.app>
2024-03-18 16:04:55 +0100

    Format files

--- vite.config.ts
+++ vite.config.ts
@@ -1,27 +1,27 @@
 /// <reference types="vitest" />
 /// <reference types="vite/client" />
 
-import { defineConfig } from 'vite';
-import tsconfigPaths from 'vite-tsconfig-paths';
-import { vitePlugin as remix } from '@remix-run/dev';
-import { vercelPreset } from '@vercel/remix/vite';
-import { installGlobals } from '@remix-run/node';
+import { defineConfig } from "vite";
+import tsconfigPaths from "vite-tsconfig-paths";
+import { vitePlugin as remix } from "@remix-run/dev";
+import { vercelPreset } from "@vercel/remix/vite";
+import { installGlobals } from "@remix-run/node";
 
 installGlobals();
 
 export default defineConfig({
-	server: {
-		port: 3000,
-	},
-	plugins: [
-		remix({
-			ignoredRouteFiles: ['**/.*'],
-			// appDirectory: "app",
-			// assetsBuildDirectory: "public/build",
-			// serverBuildPath: "api/index.js",
-			// publicPath: "/build/",
-			presets: [vercelPreset()],
-		}),
-		tsconfigPaths(),
-	],
+  server: {
+    port: 3000,
+  },
+  plugins: [
+    remix({
+      ignoredRouteFiles: ["**/.*"],
+      // appDirectory: "app",
+      // assetsBuildDirectory: "public/build",
+      // serverBuildPath: "api/index.js",
+      // publicPath: "/build/",
+      presets: [vercelPreset()],
+    }),
+    tsconfigPaths(),
+  ],
 });
