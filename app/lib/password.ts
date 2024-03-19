a62da0d5249158479eff2036bb38f29bf69dae35
Rickard Lindahl <rickard@lindahl.app>
2024-03-19 16:26:03 +0100

    Add lib/password helpers

---
+++ app/lib/password.ts
@@ -0,0 +1,17 @@
+export async function hashPassword(password: string) {
+  const pwUtf8 = new TextEncoder().encode(password);
+
+  const pwHashBuffer = await crypto.subtle.digest("SHA-256", pwUtf8);
+
+  const pwHashHexString = Array.from(new Uint8Array(pwHashBuffer))
+    .map(byte => byte.toString(16).padStart(2, "0"))
+    .join("");
+
+  return pwHashHexString;
+}
+
+export async function comparePasswords(plainPassword: string, hashedPassword: string) {
+  const hashedPlainPassword = await hashPassword(plainPassword);
+
+  return hashedPlainPassword === hashedPassword;
+}
