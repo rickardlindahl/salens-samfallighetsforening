2772aaf85970edea4c30602539b1643720456dff
Rickard Lindahl <rickard@lindahl.app>
2024-03-22 08:16:58 +0100

    Add loginSchema to auth.server

--- app/services/auth.server.ts
+++ app/services/auth.server.ts
@@ -4,17 +4,23 @@ import { FormStrategy } from "remix-auth-form";
 import { eq } from "drizzle-orm";
 import { db } from "~/db";
 import { users, type User } from "~/db/schema";
-import { comparePasswords, hashPassword } from "~/lib/password";
+import { comparePasswords } from "~/lib/password";
+import { z } from "zod";
 
 // Create an instance of the authenticator, pass a generic with what
 // strategies will return and will store in the session
 export let authenticator = new Authenticator<User>(sessionStorage);
 
+const loginSchema = z.object({
+  email: z.string().email(),
+  password: z.string(),
+});
+
 // Tell the Authenticator to use the form strategy
 authenticator.use(
   new FormStrategy(async ({ form }) => {
-    const email = form.get("email");
-    const password = form.get("password");
+    const { email, password } = loginSchema.parse(form);
+
     const user = await login(email as string, password as string);
 
     // the type of this user must match the type you pass to the Authenticator
