cdbe44792e77c5b44eee3821d456d6ea5d762057
Rickard Lindahl <rickard@lindahl.app>
2024-03-27 07:02:12 +0100

    Add posts route

---
+++ app/routes/posts.tsx
@@ -0,0 +1,53 @@
+import { Await, useLoaderData } from "@remix-run/react";
+import { defer } from "@vercel/remix";
+import { Suspense } from "react";
+import { db } from "~/db";
+import { posts as postsTable } from "~/db/schema";
+
+export const config = { runtime: "edge" };
+
+export const meta = () => [
+  {
+    title: "Remix DnB Stack | Books Edge Streaming",
+  },
+  {
+    charset: "utf-8",
+  },
+  {
+    viewport: "width=device-width,initial-scale=1",
+  },
+];
+
+function getPosts() {
+  return db.select().from(postsTable).all();
+}
+
+export const loader = async () => {
+  return defer({
+    posts: getPosts(),
+  });
+};
+
+export default function Posts() {
+  const { posts } = useLoaderData<typeof loader>();
+
+  return (
+    <main>
+      <h1>Posts</h1>
+      <Suspense fallback={<h1 className="mb-10 mt-5 text-center text-3xl font-bold">Loading books ...</h1>}>
+        <Await resolve={posts}>
+          {posts => (
+            <div>
+              {posts.map(post => (
+                <div key={post.id} className="mb-5">
+                  <h2 className="text-2xl font-bold">{post.title}</h2>
+                  <p>{post.body}</p>
+                </div>
+              ))}
+            </div>
+          )}
+        </Await>
+      </Suspense>
+    </main>
+  );
+}
