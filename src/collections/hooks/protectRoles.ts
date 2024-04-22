c047922e993b43bee7e3e578ebaaabff42de81a3
Rickard Lindahl <rickard@lindahl.app>
2024-04-22 22:00:53 +0200

    Update hooks

--- src/collections/hooks/protectRoles.ts
+++ src/collections/hooks/protectRoles.ts
@@ -1,18 +1,25 @@
+import { User } from "@/payload-types";
 import type { FieldHook } from "payload/types";
 
 // ensure there is always a `user` role
 // do not let non-admins change roles
-export const protectRoles: FieldHook<User & { id: string }> = async ({
+export const protectRoles: FieldHook<User, User["roles"]> = async ({
 	req,
-	data,
+	value,
+	operation,
 }) => {
+	if (operation !== "update") {
+		return value;
+	}
+
 	const isAdmin = req.user?.roles?.includes("admin");
 
 	if (!isAdmin) {
 		return ["user"];
 	}
 
-	const userRoles = new Set(data?.roles || []);
+	const userRoles = new Set(value || []);
 	userRoles.add("user");
-	return [...userRoles];
+
+	return Array.from(userRoles.values());
 };
