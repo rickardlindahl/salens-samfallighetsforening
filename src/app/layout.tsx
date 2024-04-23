71666438b9b233d77bad6609ebf482ca6d093d82
Rickard Lindahl <rickard@lindahl.app>
2024-04-23 13:58:34 +0200

    Add basic index page

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,12 +1,14 @@
 import React from "react";
 import { AuthProvider } from "@/lib/providers/Auth";
 
-export default function RootLayout({ children }: { children: React.ReactNode }) {
+export default function RootLayout({
+	children,
+}: { children: React.ReactNode }) {
 	return (
-    <html lang="en">
-      <body>
-        <AuthProvider api="rest">{children}</AuthProvider>
-      </body>
-    </html>
-  );
+		<html lang="en">
+			<body>
+				<AuthProvider api="rest">{children}</AuthProvider>
+			</body>
+		</html>
+	);
 }
