4ec3cea2707c715893f2cb4d01abe3c433954c43
Rickard Lindahl <rickard@lindahl.app>
2024-05-02 21:10:21 +0200

    Add vercel blob storage

--- src/payload.config.ts
+++ src/payload.config.ts
@@ -1,5 +1,5 @@
 import { postgresAdapter } from "@payloadcms/db-postgres";
-// import { payloadCloud } from '@payloadcms/plugin-cloud'
+import { vercelBlobStorage } from "@payloadcms/storage-vercel-blob";
 import { lexicalEditor } from "@payloadcms/richtext-lexical"; // editor-import
 import path from "path";
 import { buildConfig } from "payload/config";
@@ -10,74 +10,84 @@ import { sv } from "payload/i18n/sv";
 
 import { Users } from "@/collections/Users";
 import { Posts } from "@/collections/Posts";
+import { Media } from "@/collections/Media";
 
 const filename = fileURLToPath(import.meta.url);
 const dirname = path.dirname(filename);
 
 export default buildConfig({
-	admin: {
-		user: Users.slug,
-	},
-	collections: [Users, Posts],
-	editor: lexicalEditor({}),
-	// plugins: [payloadCloud()], // TODO: Re-enable when cloud supports 3.0
-	secret: process.env.PAYLOAD_SECRET || "",
-	typescript: {
-		outputFile: path.resolve(dirname, "payload-types.ts"),
-	},
-	db: postgresAdapter({
-		pool: {
-			connectionString: process.env.DATABASE_URI || "",
-		},
-	}),
-	i18n: {
-		supportedLanguages: { en, sv },
-		fallbackLanguage: "en",
-	},
-	// Sharp is now an optional dependency -
-	// if you want to resize images, crop, set focal point, etc.
-	// make sure to install it and pass it to the config.
+  admin: {
+    user: Users.slug,
+  },
+  collections: [Users, Posts, Media],
+  editor: lexicalEditor({}),
+  plugins: [
+    vercelBlobStorage({
+      collections: {
+        [Media.slug]: true,
+      },
+      token: process.env.BLOB_READ_WRITE_TOKEN || '',
+    }),
 
-	// This is temporary - we may make an adapter pattern
-	// for this before reaching 3.0 stable
+  ],
+  secret: process.env.PAYLOAD_SECRET || "",
+  typescript: {
+    outputFile: path.resolve(dirname, "payload-types.ts"),
+  },
+  db: postgresAdapter({
+    pool: {
+      connectionString: process.env.DATABASE_URI || "",
+    },
+  }),
+  i18n: {
+    supportedLanguages: { en, sv },
+    fallbackLanguage: "en",
+  },
+  // Sharp is now an optional dependency -
+  // if you want to resize images, crop, set focal point, etc.
+  // make sure to install it and pass it to the config.
 
-	// sharp,
-	//
-	onInit: async (payload) => {
-		if (process.env.NODE_ENV === "development") {
-			console.log("onInit");
+  // This is temporary - we may make an adapter pattern
+  // for this before reaching 3.0 stable
 
-			const numberOfUsers = (
-				await payload.find<"users">({ collection: "users", limit: 2 })
-			).totalDocs;
+  // sharp,
+  //
+  //
+  onInit: async (payload) => {
+    if (process.env.NODE_ENV === "development") {
+      console.log("onInit");
 
-			if (numberOfUsers === 0) {
-				console.log("Creating user with admin role");
-				await payload.create({
-					collection: "users",
-					data: {
-						email: "admin@salen.com",
-						password: "password",
-						roles: ["admin"],
-						firstName: "Admin",
-						lastName: "Adminsson",
-					},
-				});
+      const numberOfUsers = (
+        await payload.find<"users">({ collection: "users", limit: 2 })
+      ).totalDocs;
 
-				console.log("Creating user with user role");
-				await payload.create({
-					collection: "users",
-					data: {
-						email: "user@salen.com",
-						password: "password",
-						roles: ["user"],
-						firstName: "User",
-						lastName: "Usersson",
-					},
-				});
-			}
-		}
-		/*
+      if (numberOfUsers === 0) {
+        console.log("Creating user with admin role");
+        await payload.create({
+          collection: "users",
+          data: {
+            email: "admin@salen.com",
+            password: "password",
+            roles: ["admin"],
+            firstName: "Admin",
+            lastName: "Adminsson",
+          },
+        });
+
+        console.log("Creating user with user role");
+        await payload.create({
+          collection: "users",
+          data: {
+            email: "user@salen.com",
+            password: "password",
+            roles: ["user"],
+            firstName: "User",
+            lastName: "Usersson",
+          },
+        });
+      }
+    }
+    /*
     await mapAsync([...Array(11)], async () => {
       await payload.create({
         collection: "posts",
@@ -89,7 +99,7 @@ export default buildConfig({
       });
     });
   */
-	},
+  },
 });
 
 /*
