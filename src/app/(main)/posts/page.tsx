e669838c55dca1e1b7b0e9c40b3a838ec4b3fa45
Rickard Lindahl <rickard@lindahl.app>
2024-05-06 15:53:14 +0200

    Add posts page

---
+++ src/app/(main)/posts/page.tsx
@@ -0,0 +1,25 @@
+import { getPayload } from "payload";
+import configPromise from "@payload-config";
+
+async function getPosts() {
+  const payload = await getPayload({
+    config: configPromise,
+  });
+
+  return payload.find({
+    collection: "posts",
+  });
+}
+
+export default async function Posts() {
+  const posts = await getPosts();
+
+  return (
+    <div>
+      <h1>Posts</h1>
+      {posts.docs.map((post) => (
+        <div key={post.id}>{post.title}</div>
+      ))}
+    </div>
+  );
+}
