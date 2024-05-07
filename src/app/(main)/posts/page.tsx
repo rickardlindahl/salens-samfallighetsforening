1cfa5cefc3f7ac2e71e08606e685727fc2720228
Rickard Lindahl <rickard@lindahl.app>
2024-05-07 07:50:28 +0200

    Format posts page

--- src/app/(main)/posts/page.tsx
+++ src/app/(main)/posts/page.tsx
@@ -2,24 +2,24 @@ import { getPayload } from "payload";
 import configPromise from "@payload-config";
 
 async function getPosts() {
-  const payload = await getPayload({
-    config: configPromise,
-  });
+	const payload = await getPayload({
+		config: configPromise,
+	});
 
-  return payload.find({
-    collection: "posts",
-  });
+	return payload.find({
+		collection: "posts",
+	});
 }
 
 export default async function Posts() {
-  const posts = await getPosts();
+	const posts = await getPosts();
 
-  return (
-    <div>
-      <h1>Posts</h1>
-      {posts.docs.map((post) => (
-        <div key={post.id}>{post.title}</div>
-      ))}
-    </div>
-  );
+	return (
+		<div>
+			<h1>Posts</h1>
+			{posts.docs.map((post) => (
+				<div key={post.id}>{post.title}</div>
+			))}
+		</div>
+	);
 }
