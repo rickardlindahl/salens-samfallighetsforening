684cdeeb642c942cd0c7646433a6d135e9fb94d2
Rickard Lindahl <rickard@lindahl.app>
2024-05-08 16:10:04 +0200

    Move mode toggle to end on mobile

--- src/components/site-header.tsx
+++ src/components/site-header.tsx
@@ -23,10 +23,8 @@ export function SiteHeader() {
 			<div className="container flex h-14 max-w-screen-2xl items-center">
 				<MainNav routeLinks={routeLinks} />
 				<MobileNav routeLinks={routeLinks} />
-				<div className="flex flex-1 items-center justify-between space-x-2 md:justify-end">
-					<nav className="flex items-center">
-						<ModeToggle />
-					</nav>
+				<div className="flex flex-1 space-x-2 justify-end">
+					<ModeToggle />
 				</div>
 			</div>
 		</header>
