c11a334f6fa37f2a71af7ea80354d8f562b13a1f
Rickard Lindahl <rickard@lindahl.app>
2024-05-13 15:11:16 +0200

    Replace login page with logout effect + navigate

--- src/app/(main)/(auth)/logout/page.tsx
+++ src/app/(main)/(auth)/logout/page.tsx
@@ -1,7 +1,27 @@
-import React from "react";
+"use client";
 
-import { LogoutForm } from "./logout-form";
+import { useEffect } from "react";
 
-export default async function Logout() {
-	return <LogoutForm />;
+import { useAuth } from "@/lib/providers/Auth";
+import { useRouter } from "next/navigation";
+import { toast } from "sonner";
+
+export default function LogoutPage() {
+	const router = useRouter();
+	const { logout } = useAuth();
+
+	useEffect(() => {
+		const performLogout = async () => {
+			try {
+				await logout();
+			} catch (_) {}
+			toast.success("Logged out successfully");
+
+			router.push("/");
+		};
+
+		performLogout();
+	}, [logout, router]);
+
+	return null;
 }
