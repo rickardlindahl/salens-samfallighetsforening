bf3e0698fa730c8624e0ae43d9ffec8550e53c49
Rickard Lindahl <rickard@lindahl.app>
2024-05-14 09:12:20 +0200

    Update post styling

--- src/app/(main)/(dashboard)/posts/post.tsx
+++ src/app/(main)/(dashboard)/posts/post.tsx
@@ -1,11 +1,39 @@
 import { RichText } from "@/components/rich-text";
-import { type Post } from "@/payload-types";
+import { formatDate } from "@/lib/utils";
+import type { Post as IPost, Media } from "@/payload-types";
+import Image from "next/image";
 
-export async function Post({ post }: { post: Post }) {
+export function Post({ post, priority }: { post: IPost; priority: boolean }) {
 	return (
-		<>
-			<h2>{post.title}</h2>
+		<article key={post.id} className="group relative flex flex-col space-y-2">
+			<PostImage image={post.image} priority={priority} />
+			<h2 className="text-2xl font-extrabold">{post.title}</h2>
+			<p className="text-sm text-muted-foreground">
+				<time dateTime={post.createdAt}>{formatDate(post.createdAt)}</time>
+			</p>
 			<RichText content={post.content} className="w-full" />
-		</>
+		</article>
+	);
+}
+
+type PostImageType = number | Media | null | undefined;
+
+function PostImage({
+	image,
+	priority,
+}: { image: PostImageType; priority: boolean }) {
+	if (!image || typeof image === "number" || !image.url) {
+		return null;
+	}
+
+	return (
+		<Image
+			src={image.url}
+			alt={image.alt}
+			width={804}
+			height={452}
+			className="rounded-md border bg-muted transition-colors"
+			priority={priority}
+		/>
 	);
 }
