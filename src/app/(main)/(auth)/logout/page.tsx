21395ba5ca215c172a84bced709c02a09728191f
Rickard Lindahl <rickard@lindahl.app>
2024-05-14 15:54:40 +0200

    Render logout screen with text and spinner

--- src/app/(main)/(auth)/logout/page.tsx
+++ src/app/(main)/(auth)/logout/page.tsx
@@ -2,6 +2,7 @@
 
 import { useEffect } from "react";
 
+import * as Icons from "@/components/icons";
 import { useAuth } from "@/lib/providers/Auth";
 import { useRouter } from "next/navigation";
 import { toast } from "sonner";
@@ -23,5 +24,18 @@ export default function LogoutPage() {
 		performLogout();
 	}, [logout, router]);
 
-	return null;
+	return (
+		<>
+			<div className="flex flex-col space-y-2 text-center">
+				<Icons.Logo className="mx-auto h-6 w-6" />
+				<h1 className="text-2xl font-semibold tracking-tight">Log out</h1>
+				<p className="text-sm text-muted-foreground">
+					Please wait while you're being logged out.
+				</p>
+				<div className="mx-auto">
+					<Icons.Spinner className="mr-2 h-4 w-4 animate-spin" />
+				</div>
+			</div>
+		</>
+	);
 }
