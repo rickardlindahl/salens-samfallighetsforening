e7f32506ebdb418cb759b524b8dcb60ae4d44126
Rickard Lindahl <rickard@lindahl.app>
2024-05-21 13:38:14 +0200

    Display users in table

--- src/app/admin/users/page.tsx
+++ src/app/admin/users/page.tsx
@@ -9,41 +9,65 @@ import {
 } from "@/components/ui/card";
 import AdminSubLayout from "../admin-sub-layout";
 import { routeLinks } from "./config";
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
 
 export default async function AdminUsersPage() {
   return (
     <AdminSubLayout routeLinks={routeLinks} title="Users">
-      <>
-        <Card x-chunk="dashboard-04-chunk-1">
-          <CardHeader>
-            <CardTitle>Users</CardTitle>
-            <CardDescription>
-              Used to identify your store in the marketplace.
-            </CardDescription>
-          </CardHeader>
-          <CardContent>
-            <div>hejmsan</div>
-          </CardContent>
-          <CardFooter className="border-t px-6 py-4">
-            <Button>Save</Button>
-          </CardFooter>
-        </Card>
-        <Card x-chunk="dashboard-04-chunk-2">
-          <CardHeader>
-            <CardTitle>Plugins Directory</CardTitle>
-            <CardDescription>
-              The directory within your project, in which your plugins are
-              located.
-            </CardDescription>
-          </CardHeader>
-          <CardContent>
-            <div>lalala</div>
-          </CardContent>
-          <CardFooter className="border-t px-6 py-4">
-            <Button>Save</Button>
-          </CardFooter>
-        </Card>
-      </>
+      <Card x-chunk="dashboard-04-chunk-1">
+        <CardHeader>
+          <CardTitle>Users</CardTitle>
+          <CardDescription>
+            Used to identify your store in the marketplace.
+          </CardDescription>
+        </CardHeader>
+        <CardContent>
+          <Table>
+            <TableHeader>
+              <TableRow>
+                <TableHead>Name</TableHead>
+                <TableHead className="hidden sm:table-cell">Email</TableHead>
+                <TableHead className="text-right">Role</TableHead>
+              </TableRow>
+            </TableHeader>
+            <TableBody>
+              {[
+                {
+                  id: "hej",
+                  name: "Admin Person",
+                  email: "tmp@example.com",
+                  role: "admin",
+                },
+                {
+                  id: "hej",
+                  name: "User Person",
+                  email: "tmp2@example.com",
+                  role: "role",
+                },
+              ].map((user) => (
+                <TableRow key={user.id} className="bg-accent">
+                  <TableCell>
+                    <div className="font-medium">{user.name}</div>
+                  </TableCell>
+                  <TableCell className="hidden sm:table-cell">
+                    <div className="hidden text-sm text-muted-foreground md:inline">
+                      {user.email}
+                    </div>
+                  </TableCell>
+                  <TableCell className="text-right">{user.role}</TableCell>
+                </TableRow>
+              ))}
+            </TableBody>
+          </Table>
+        </CardContent>
+      </Card>
     </AdminSubLayout>
   );
 }
