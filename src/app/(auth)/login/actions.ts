b56ad829082420e57b348ba0d7daeaf1094496a1
Rickard Lindahl <rickard@lindahl.app>
2024-06-10 23:08:58 +0200

    Fix login redirect

--- src/app/(auth)/login/actions.ts
+++ src/app/(auth)/login/actions.ts
@@ -3,21 +3,36 @@
 import { signIn } from "@/auth";
 import { InvalidLoginError } from "@/auth.config";
 import { revalidatePath } from "next/cache";
+import { redirect } from "next/navigation";
 import type { LoginFormData } from "./schema";
-import { isRedirectError } from "next/dist/client/components/redirect";
 
 export async function loginAction(credentials: LoginFormData) {
+  let redirectUrl: string | null = null;
+
   try {
-    await signIn("credentials", credentials);
-  } catch (e) {
-    if (isRedirectError(e)) {
-      revalidatePath("/");
-      throw e;
+    const url = await signIn("credentials", {
+      redirect: false,
+      ...credentials,
+    });
+
+    if (url) {
+      const searchParams = new URLSearchParams(url.split("?")[1]);
+      redirectUrl = searchParams.get("callbackUrl") ?? "/";
     }
-    console.error(e);
+  } catch (e) {
     return {
       isError: true,
       isInvalidLoginError: e instanceof InvalidLoginError,
+    };
+  }
+
+  if (redirectUrl) {
+    revalidatePath("/");
+    redirect(redirectUrl);
+  } else {
+    return {
+      isError: true,
+      isInvalidLoginError: false,
     };
   }
 }
