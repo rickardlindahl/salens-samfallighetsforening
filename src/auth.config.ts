9039238a6f8fd39e20f8b753c693e52801d3e57a
Rickard Lindahl <rickard@lindahl.app>
2024-06-10 12:11:17 +0200

    Update nextauth stuff

--- src/auth.config.ts
+++ src/auth.config.ts
@@ -19,6 +19,7 @@ export const authConfig = {
     // added later in auth.ts since it requires bcrypt which is only compatible with Node.js
     // while this file is also used in non-Node.js environments
   ],
+  trustHost: true,
   callbacks: {
     authorized({ auth, request: { nextUrl } }) {
       const user = auth?.user;
