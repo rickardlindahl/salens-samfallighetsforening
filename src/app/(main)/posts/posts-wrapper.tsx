6fc708cc75ae588df9042126383cd7aaf03ae5ef
Rickard Lindahl <rickard@lindahl.app>
2024-06-11 23:00:16 +0200

    Fix no posts yet style

--- src/app/(main)/posts/posts-wrapper.tsx
+++ src/app/(main)/posts/posts-wrapper.tsx
@@ -24,7 +24,7 @@ export async function PostsWrapper() {
 
   if (posts.length === 0) {
     return (
-      <div className="flex flex-col items-center gap-1 text-center">
+      <div>
         <h3 className="text-2xl font-bold tracking-tight">
           Inga inlägg skapade ännu
         </h3>
--- src/app/admin/posts/posts-table.tsx
+++ src/app/admin/posts/posts-table.tsx
@@ -22,8 +22,8 @@ export async function PostsTable() {
 
   if (posts.length === 0) {
     return (
-      <div className="flex flex-col items-center gap-1 text-center">
-        <h3 className="text-2xl font-bold tracking-tight">
+      <div className="flex flex-col gap-1">
+        <h3 className="text-xl font-bold tracking-tight">
           Inga inlägg skapade än
         </h3>
         <p className="text-sm text-muted-foreground">
