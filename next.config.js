0c40f47c17fc47b3adb9263352c73bd578fca4ad
Rickard Lindahl <rickard@lindahl.app>
2024-06-14 10:20:22 +0200

    Add plausible proxy to avoid it being adblocked

--- next.config.js
+++ next.config.js
@@ -1,11 +1,15 @@
 import { fileURLToPath } from "node:url";
 import createJiti from "jiti";
+import { withPlausibleProxy } from "next-plausible";
 const jiti = createJiti(fileURLToPath(import.meta.url));
 
 // Import env here to validate during build. Using jiti we can import .ts files :)
 jiti("./src/env");
 
 /** @type {import('next').NextConfig} */
-export default {
+const nextConfig = withPlausibleProxy({
+  scriptName: "plsbl",
+})({
   /** ... */
-};
+});
+export default nextConfig;
