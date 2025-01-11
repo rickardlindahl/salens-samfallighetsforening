9adbe31a3c7287032fa35aa6f873e035eb394b32
Rickard Lindahl <rickard@lindahl.app>
2025-01-11 14:35:01 +0100

    Add prodMigrations so migrations are run on startup

--- src/payload.config.ts
+++ src/payload.config.ts
@@ -11,6 +11,7 @@ import { Media } from "./collections/Media";
 import { Posts } from "./collections/Posts";
 import { Documents } from "./collections/Documents";
 import { createEmailTransport } from "./lib/email";
+import { migrations } from "./migrations";
 
 const filename = fileURLToPath(import.meta.url);
 const dirname = path.dirname(filename);
@@ -32,6 +33,7 @@ export default buildConfig({
 		pool: {
 			connectionString: process.env.POSTGRES_URL || "",
 		},
+		prodMigrations: migrations,
 	}),
 	email: process.env.SMTP_HOST
 		? nodemailerAdapter({
@@ -43,6 +45,7 @@ export default buildConfig({
 		: undefined,
 	plugins: [
 		vercelBlobStorage({
+			enabled: true,
 			collections: {
 				media: {
 					prefix: "/media",
